0a3d2668b9de902dd9a1c5ff486b17dff75c56b8
mazed2020 <mazedulislam959@gmail.com>
2024-11-09 00:42:41 +0600

    add package

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import connectDB from "./db/connection.js";
+import app from './app.js'
+import dotenv from 'dotenv'
+dotenv.config({
+    path: './.env'
+})
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`apps running on ${process.env.PORT}`);
+    }) 
+}).catch((err)=>{
+    console.log(`db connenction error is ${err}`);
+
+})
