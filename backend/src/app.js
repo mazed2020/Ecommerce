be4d6689c428b5362a3445f3a41ac3c455d72e71
mazed2020 <mazedulislam959@gmail.com>
2024-11-09 00:47:17 +0600

    please readd

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,22 @@ import express from 'express';
 const app = express();
 
 
+/*+++++++++++++++basic configaration start here+++++++++++++++++ */
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}));
+//json body limit accept
+app.use(express.json({limit:'16kb'}));
+// request for url
+app.use(express.urlencoded({
+    extended:true,
+    limit:'16kb',
+}));
+//uploaded file will be store the folder
+app.use(express.static("public"));
+app.use(cookiesParser());
+
  
 
 
